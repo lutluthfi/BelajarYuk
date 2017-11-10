09656cd70d142ad3b4c6c7f80a7eec77ebaecd8f
Rama Zeta <ramazeta1997@gmail.com>
2017-11-11 03:13:48 +0700

    Change Notification Handler

--- app/src/main/java/com/example/arifluthfiansyah/belajaryuk/notification/NotificationReceivedHandler.java
+++ app/src/main/java/com/example/arifluthfiansyah/belajaryuk/notification/NotificationReceivedHandler.java
@@ -1,8 +1,26 @@
 package com.example.arifluthfiansyah.belajaryuk.notification;
 
+import android.util.Log;
+
+import com.onesignal.OSNotification;
+import com.onesignal.OneSignal;
+
+import org.json.JSONObject;
+
 /**
  * Created by rama on 11/11/17.
  */
 
-public class NotificationReceivedHandler {
+public class NotificationReceivedHandler implements OneSignal.NotificationReceivedHandler {
+    @Override
+    public void notificationReceived(OSNotification notification) {
+        JSONObject data = notification.payload.additionalData;
+        String customKey;
+
+        if (data != null) {
+            customKey = data.optString("customkey", null);
+            if (customKey != null)
+                Log.i("OneSignalExample", "customkey set with value: " + customKey);
+        }
+    }
 }
